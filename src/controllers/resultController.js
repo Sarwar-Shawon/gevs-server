352db3cf4a9455c3761249d3b627ca42604c7da7
Sarwar-Shawon <sshawon95@gmail.com>
2024-01-05 00:23:44 +0000

    wip... calculate vote result

--- src/controllers/resultController.js
+++ src/controllers/resultController.js
@@ -41,12 +41,22 @@ const getVoteResult = async (req, res) => {
       const { party } = winner;
       partySeats[party] = (partySeats[party] || 0) + 1;
     });
-    console.log("partySeats", partySeats);
+    // console.log("partySeats", partySeats);
+    //calculate total seats
+    const totalSeats = Object.values(partySeats).reduce(
+      (total, seats) => total + seats,
+      0
+    );
+    const maxSeats = Math.max(...Object.values(partySeats));
+    let hung_parliament = false;
+    if (maxSeats / totalSeats <= 0.5) hung_parliament = true;
+    // console.log("totalSeats:::", totalSeats);
+    // console.log("maxSeats:::", maxSeats);
     //calculate winning party
     const winningParty = Object.keys(partySeats).reduce((a, b) =>
       partySeats[a] > partySeats[b] ? a : b
     );
-    console.log("winningParty", winningParty);
+    // console.log("winningParty", winningParty);
     //
     const electionSettings = await ElectionSettings.findOne({
       settingsId: "Shangri-La-Election",
@@ -56,16 +66,12 @@ const getVoteResult = async (req, res) => {
       electionSettings.status == "not-started"
         ? true
         : false;
-
     const winnerStatus = isElectionOngoing
       ? "Pending"
-      : Object.values(partySeats).some((seatCount) => {
-          console.log("seatCount:::", seatCount);
-          seatCount >= candidates.length / 2;
-        })
-      ? winningParty
-      : "Hung Parliament";
-    console.log("winnerStatus::::", winnerStatus);
+      : hung_parliament
+      ? "Hung Parliament"
+      : winningParty;
+    // console.log("winnerStatus::::", winnerStatus);
     const result = {
       status: isElectionOngoing ? "Pending" : "Completed",
       winner: winnerStatus,
@@ -74,80 +80,8 @@ const getVoteResult = async (req, res) => {
         seat: seat.toString(),
       })),
     };
-    console.log("output", result);
+    // console.log("result", result);
     res.send({ status: "success", data: result });
-    // Calculate seats and vote count for each candidate in each constituency
-    // const seatsByConstituency = {};
-    // Object.entries(candidatesByConstituency).forEach(
-    //   ([constituencyId, constituencyCandidates]) => {
-    //     const seats = constituencyCandidates.reduce(
-    //       (acc, candidate) => {
-    //         // Calculate vote count for each candidate in the constituency
-    //         if (!acc.voteCountByCandidate[candidate._id.toString()]) {
-    //           acc.voteCountByCandidate[candidate._id.toString()] = 0;
-    //         }
-    //         const voteCount = candidate.vote_count || 0;
-    //         acc.voteCountByCandidate[candidate._id.toString()] += voteCount;
-
-    //         // Calculate seats for each party in the constituency
-    //         acc.partySeats[candidate.party_name] =
-    //           (acc.partySeats[candidate.party_name] || 0) + 1;
-
-    //         return acc;
-    //       },
-    //       { partySeats: {}, voteCountByCandidate: {} }
-    //     );
-    //     seatsByConstituency[constituencyId] = seats;
-    //   }
-    // );
-    // console.log("seatsByConstituency", seatsByConstituency);
-
-    // // Calculate overall seats and vote count for each candidate
-    // const overallSeats = {};
-    // const overallVoteCountByCandidate = {};
-    // Object.entries(seatsByConstituency).forEach(
-    //   ([constituencyId, constituencySeats]) => {
-    //     Object.entries(constituencySeats.voteCountByCandidate).forEach(
-    //       ([candidateId, voteCount]) => {
-    //         if (!overallSeats[candidateId]) {
-    //           overallSeats[candidateId] = 0;
-    //         }
-    //         overallSeats[candidateId] += voteCount;
-
-    //         if (!overallVoteCountByCandidate[candidateId]) {
-    //           overallVoteCountByCandidate[candidateId] = 0;
-    //         }
-    //         overallVoteCountByCandidate[candidateId] += voteCount;
-    //       }
-    //     );
-    //   }
-    // );
-    // console.log("overallSeats::::", overallSeats);
-    // console.log("overallVoteCountByCandidate::::", overallVoteCountByCandidate);
-    // // Determine the overall winner and if there is a majority
-    // const overallWinner = Object.keys(overallSeats).reduce((a, b) =>
-    //   overallSeats[a] > overallSeats[b] ? a : b
-    // );
-    // const totalSeats = Object.values(overallSeats).reduce(
-    //   (acc, seat) => acc + seat,
-    //   0
-    // );
-    // const majorityThreshold = totalSeats / 2;
-    // const isMajority = overallSeats[overallWinner] > majorityThreshold;
-
-    // // Construct the result object in the specified format
-    // const result = {
-    //   status: isMajority ? "Completed" : "Hung Parliament",
-    //   winner: isMajority ? overallWinner : "No Majority",
-    //   seats: Object.entries(overallSeats).map(([candidateId, seat]) => ({
-    //     party: candidates.find((c) => c._id.toString() === candidateId)
-    //       .party_name,
-    //     seat: seat.toString(),
-    //   })),
-    // };
-
-    // console.log(result);
-    // res.send({ status: "success", result: result });
   } catch (err) {
     console.error("Error calculating election result:", err);
     res.send({ status: "err", message: err?.message });
